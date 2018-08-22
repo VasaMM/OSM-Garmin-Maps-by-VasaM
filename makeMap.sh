4d949d4717359a2f8dbedaa33c65355a90ff4259
VasaMM <vasa@geocaching.cz>
2018-08-22 13:45:25 +0200

    Úprava generovacího skriptu
    Instalační dávka kontroluje práva spráce
    Změna poskytovatele výškových dat - rychlejší generování, přesnější
    vrstevnice
    Oddělena definice mapových oblastí do vlastního souboru

--- makeMap.sh
+++ makeMap.sh
@@ -9,18 +9,18 @@ set -e		# Konec v pripade chyby
 # PASSWORD=Password
 
 # Nactu prihlasovaci udaje ze souboru username a password pokud existuji
-if [ -f ./username ]; then
-	USERNAME=`cat username`
-fi
+# if [ -f ./username ]; then
+# 	USERNAME=`cat username`
+# fi
 
-if [ -f ./password ]; then
-	PASSWORD=`cat password`
-fi
+# if [ -f ./password ]; then
+# 	PASSWORD=`cat password`
+# fi
 
 # Dalsi nastevni
 PYTHON=python3.5
-JAVAMEM=-Xmx8000m
-VERSION=40
+JAVAMEM=-Xmx8000m		# Maximalni velikost RAM, kterou lze pouzit
+VERSION=42
 
 
 # Ukoncovaci funkce
@@ -49,20 +49,22 @@ function viewHelp {
 	echo "  Skript je nezvykle ukecany (do budoucna je v planu i \"ticha\" verze) a na zacatku spusteni se uzivatele pta, co chce udelat. Proto jej staci spustit bez parametru."
 	echo "  Pro bezobsluzne automaticke spousteni lze chovani ovlivnit pomoci parametru:"
 	echo "    * -a <stat> | --area <stat> definuje stat/oblast, pro kterou je mapa generovana. Viz seznam statu."
-	echo "    * -dy | --download_yes vynuti vzdy nové stažení mapových dat"
+	echo "    * -dy | --download_yes vynuti vzdy nove stazeni mapovych dat"
 	echo "    * -dn | --download_no v pripade, ze byli drive stazena mapova data, nebudou se znovu stahovat."
 	echo "        **POZOR**, neni provadena validace techto dat. Jedna-li se o fragment z prechoziho preruseneho stahovani, dojde k chybe."
 	echo "    * -ns | --no_split zakaze deleni mapovych souboru na mensi dily. Vhodne pouze u velmi malych oblasti a pro pocitace s dostatkem RAM."
 	echo "    * -h | --help zobrazi tuto napovedu."
 	echo ""
 	echo "## Seznam statu"
-	echo "  Staty jsou primo definovany ve skriptu. Jejich definice zacina okolo radku 120."
+	echo "  Staty jsou definovany ve skriptu states.sh."
 	echo "    * OL - Olomouc - vhodne pro testovani"
 	echo "    * CZ - Ceska republika"
 	echo "    * SK - Slovenska republika"
 	echo "    * UA - Ukrajina"
 	echo "    * RO - Rumunsko"
+	echo "    * NO - Norsko"
 	echo "    * KG - Kyrgyzstan"
+	echo "    * KZ - Kazachstan"
 }
 
 
@@ -120,112 +122,8 @@ done
 
 
 # Ziskam stat, nebyl-li zadan
-while [ true ]; do
-	if [ -z ${STATE+x} ]; then
-		echo "Zadejte zkratku statu, pro ktery chcete vytvorit mapu."
-		echo "  * Ceska republika - [CZ]"
-		echo "  * Slovenska republika - [SK]"
-		echo "  * Kyrgyzstan - [KG]"
-		echo "  * Ukrajina - [UA]"
-		echo "  * Rumunsku - [RO]"
-		echo ""
-	 	read -p "Vybrana mapa: " -r
-	 	STATE=$REPLY
-	fi
-
-	# Vytvorim odkaz na dany stat
-	case $STATE in
-		OL|ol )
-			echo "Tvorim mapu pro Olomouc"
-			STATE="OL"
-			DATA_URL=false
-			POLY_URL=false
-			COUNTRY_NAME="Olomouc - VasaM"
-			COUNTRY_ID=8800
-			break
-			;;
-
-		#Stredni Evropa ID: 8801 - 8810
-		CZ|cz )
-			echo "Tvorim mapu pro Ceskou republiku"
-			STATE="CZ"
-			DATA_URL="http://download.geofabrik.de/europe/czech-republic-latest.osm.pbf"
-			POLY_URL="http://download.geofabrik.de/europe/czech-republic.poly"
-			COUNTRY_NAME="Ceska republika - VasaM"
-			COUNTRY_ID=8801
-			break
-			;;
-
-		SK|sk )
-			echo "Tvorim mapu pro Slovensko"
-			STATE="SK"
-			DATA_URL="http://download.geofabrik.de/europe/slovakia-latest.osm.pbf"
-			POLY_URL="http://download.geofabrik.de/europe/slovakia.poly"
-			COUNTRY_NAME="Slovenska republika - VasaM"
-			COUNTRY_ID=8802
-			break
-			;;
-
-		#Ostatni Evropa ID: 8811 - 8820
-		UA|ua )
-			STATE="UA"
-			echo "Tvorim mapu pro Ukrajinu"
-			DATA_URL="http://download.geofabrik.de/europe/ukraine-latest.osm.pbf"
-			POLY_URL="http://download.geofabrik.de/europe/ukraine.poly"
-			COUNTRY_NAME="Ukrajina - VasaM"
-			COUNTRY_ID=8811
-			break
-			;;
-
-		RO|ro )
-			echo "Tvorim mapu pro Rumunsko"
-			STATE="RO"
-			DATA_URL="http://download.geofabrik.de/europe/romania-latest.osm.pbf"
-			POLY_URL="http://download.geofabrik.de/europe/romania.poly"
-			COUNTRY_NAME="Rumunsko - VasaM"
-			COUNTRY_ID=8812
-			break
-			;;
-
-
-		#Ostatni ID: 8821 - 8890
-		KG|kg )
-			echo "Tvorim mapu pro Kyrgyzstan"
-			STATE="KG"
-			DATA_URL="http://download.geofabrik.de/asia/kyrgyzstan-latest.osm.pbf"
-			POLY_URL="http://download.geofabrik.de/asia/kyrgyzstan.poly"
-			COUNTRY_NAME="Kyrgyzstan - VasaM"
-			COUNTRY_ID=8821
-			break
-			;;
-
-
-		KZ|kz )
-			echo "Tvorim mapu pro Kazachstan"
-			STATE="KZ"
-			DATA_URL="http://download.geofabrik.de/asia/kazakhstan-latest.osm.pbf"
-			POLY_URL="http://download.geofabrik.de/asia/kazakhstan.poly"
-			COUNTRY_NAME="Kazachstan - VasaM"
-			COUNTRY_ID=8822
-			break
-			;;
-
-
-		TEST|test )
-			echo "Testuji"
-			STATE="TEST"
-			DATA_URL=false
-			POLY_URL=false
-			COUNTRY_NAME="Test - VasaM"
-			COUNTRY_ID=8899
-			break
-			;;
-
-		* )
-			echo "Neznamy stat '${STATE}'. Zkuste to znovu"
-			unset STATE
-	esac
-done
+source ./states.sh
+states
 
 
 # Zjistim, zda mam stahovat data
@@ -291,12 +189,12 @@ if [ ! -f ./pbf/$STATE-SRTM.osm.pbf ]; then
 		-j 2 \
 		-s 10 \
 		-c 200,100 \
+		--source=view3 \
 		--start-node-id=20000000000 \
 		--start-way-id=10000000000 \
 		--write-timestamp \
-		--max-nodes-per-tile=0 \
-		--earthexplorer-user=$USERNAME \
-		--earthexplorer-password=$PASSWORD
+		--max-nodes-per-tile=0
+		# --no-zero-contour
 
 	mv ./pbf/$STATE-SRTM* ./pbf/$STATE-SRTM.osm.pbf
 else
@@ -311,8 +209,7 @@ fi
 
 
 # Stahnu HGT soubory
-$PYTHON ./hgt/hgt-downloader.py ./hgt/SRTM3v3.0 ./hgt
-
+#$PYTHON ./hgt/hgt-downloader.py ./hgt/SRTM3v3.0 ./hgt
 
 
 # Generuji Mapsforge
@@ -321,13 +218,14 @@ if [ $MAPSFORGE = true ]; then
 	export JAVACMD_OPTIONS="$JAVAMEM"
 	
 	# Vlozim vrstevnice do mapy
-	if [ $DOWNLOAD = true ] || [ ! -f ../pbf/$STATE-MERGE.osm.pbf ]; then
-		./osmosis --rb file="../../pbf/$STATE.osm.pbf" --sort-0.6 --rb "../../pbf/$STATE-SRTM.osm.pbf" --sort-0.6 --merge --wb "../../pbf/$STATE-MERGE.osm.pbf"
-	fi
+	# if [ $DOWNLOAD = true ] || [ ! -f ../pbf/$STATE-MERGE.osm.pbf ]; then
+	# 	./osmosis --rb file="../../pbf/$STATE.osm.pbf" --sort-0.6 --rb "../../pbf/$STATE-SRTM.osm.pbf" --sort-0.6 --merge --wb "../../pbf/$STATE-MERGE.osm.pbf"
+	# fi
 
 	# Generuji mapu
 	# ./osmosis --rb file="../../pbf/$STATE-MERGE.osm.pbf" --mapfile-writer file="../../map/$STATE.map" type=hd preferred-languages=en,cs threads=4 tag-conf-file="../tag-mapping.xml"
-	./osmosis --rb file="../../pbf/$STATE-MERGE.osm.pbf" --mapfile-writer file="../../map/$STATE.map" type=ram preferred-languages=en tag-conf-file="../tag-mapping.xml"
+	# ./osmosis --rb file="../../pbf/$STATE-MERGE.osm.pbf" --mapfile-writer file="../../map/$STATE.map" type=ram preferred-languages=en tag-conf-file="../tag-mapping.xml"
+	./osmosis --rb file="../../pbf/$STATE.osm.pbf" --mapfile-writer file="../../map/$STATE.map" type=ram preferred-languages=en,cs,ua tag-conf-file="../tag-mapping.xml"
 
 	cd "./../.."
 else
@@ -354,7 +252,7 @@ else
 	     --mapname="${COUNTRY_ID}0001" \
 	     --overview-mapnumber="${COUNTRY_ID}0000" \
 	     --family-id="${COUNTRY_ID}" \
-	     --description="Turisticka mapa ${COUNTRY_NAME}" \
+	     --description="${COUNTRY_NAME}" \
 	     --family-name="${COUNTRY_NAME}" \
 	     --series-name="${COUNTRY_NAME}" \
 	     --country-name="${COUNTRY_NAME}" \
@@ -372,7 +270,20 @@ else
 	a=$(printf "%x\n" $COUNTRY_ID | egrep -o '[a-fA-F0-9]{2}$')
 	b=$(printf "%x\n" $COUNTRY_ID | egrep -o '^[a-fA-F0-9]{2}')
 
-	echo "echo off"                                                                                     > ./img/${STATE}_VasaM/install.bat
+
+	echo "@echo off"                                                                                    > ./img/${STATE}_VasaM/install.bat
+	echo "goto check_Permissions"                                                                      >> ./img/${STATE}_VasaM/install.bat                                   
+	echo " "                                                                                           >> ./img/${STATE}_VasaM/install.bat             
+	echo ":check_Permissions"                                                                          >> ./img/${STATE}_VasaM/install.bat                               
+	echo "net session >nul 2>&1"                                                                       >> ./img/${STATE}_VasaM/install.bat                                  
+	echo "if %errorLevel% == 0 ("                                                                      >> ./img/${STATE}_VasaM/install.bat                                   
+	echo "    goto main"                                                                               >> ./img/${STATE}_VasaM/install.bat                                 
+	echo ") else ("                                                                                    >> ./img/${STATE}_VasaM/install.bat                     
+	echo "    echo CHYBA: Spuste skript jako administrator!"                                           >> ./img/${STATE}_VasaM/install.bat                                                              
+	echo "    goto end"                                                                                >> ./img/${STATE}_VasaM/install.bat                         
+	echo ")"                                                                                           >> ./img/${STATE}_VasaM/install.bat              
+	echo " "                                                                                           >> ./img/${STATE}_VasaM/install.bat             
+	echo ":main"                                                                                       >> ./img/${STATE}_VasaM/install.bat                         
 	echo "echo - Instalce mapy do Mapsource/Basecamp"                                                  >> ./img/${STATE}_VasaM/install.bat
 	echo "echo - "                                                                                     >> ./img/${STATE}_VasaM/install.bat
 	echo "echo - Mapa:  \"${COUNTRY_NAME}\""                                                           >> ./img/${STATE}_VasaM/install.bat
@@ -398,11 +309,24 @@ else
 	echo "reg ADD %KEY%\Families\FAMILY_${COUNTRY_ID}\1 /v Bmap /t REG_SZ /d \"%~dp0mapset.img\" /f"   >> ./img/${STATE}_VasaM/install.bat
 	echo "reg ADD %KEY%\Families\FAMILY_${COUNTRY_ID}\1 /v Tdb /t REG_SZ /d \"%~dp0mapset.tdb\" /f"    >> ./img/${STATE}_VasaM/install.bat
 	echo " "                                                                                           >> ./img/${STATE}_VasaM/install.bat
+	echo ":end"                                                                                        >> ./img/${STATE}_VasaM/install.bat
 	echo "pause"                                                                                       >> ./img/${STATE}_VasaM/install.bat
 	echo "exit 0"                                                                                      >> ./img/${STATE}_VasaM/install.bat
 
 
-	echo "echo off"                                             > ./img/${STATE}_VasaM/uninstall.bat
+	echo "@echo off"                                            > ./img/${STATE}_VasaM/uninstall.bat
+	echo "goto check_Permissions"                              >> ./img/${STATE}_VasaM/uninstall.bat                                   
+	echo " "                                                   >> ./img/${STATE}_VasaM/uninstall.bat             
+	echo ":check_Permissions"                                  >> ./img/${STATE}_VasaM/uninstall.bat                               
+	echo "net session >nul 2>&1"                               >> ./img/${STATE}_VasaM/uninstall.bat                                  
+	echo "if %errorLevel% == 0 ("                              >> ./img/${STATE}_VasaM/uninstall.bat                                   
+	echo "    goto main"                                       >> ./img/${STATE}_VasaM/uninstall.bat                                 
+	echo ") else ("                                            >> ./img/${STATE}_VasaM/uninstall.bat                     
+	echo "    echo CHYBA: Spuste skript jako administrator!"   >> ./img/${STATE}_VasaM/uninstall.bat                                                              
+	echo "    goto end"                                        >> ./img/${STATE}_VasaM/uninstall.bat                         
+	echo ")"                                                   >> ./img/${STATE}_VasaM/uninstall.bat              
+	echo " "                                                   >> ./img/${STATE}_VasaM/uninstall.bat             
+	echo ":main"                                               >> ./img/${STATE}_VasaM/uninstall.bat             
 	echo "echo - Odinstalace mapy z Mapsource/Basecamp"        >> ./img/${STATE}_VasaM/uninstall.bat
 	echo "echo - "                                             >> ./img/${STATE}_VasaM/uninstall.bat
 	echo "echo - Mapa:  \"${COUNTRY_NAME}\""                   >> ./img/${STATE}_VasaM/uninstall.bat
@@ -419,6 +343,7 @@ else
 	echo ":key_ok"                                             >> ./img/${STATE}_VasaM/uninstall.bat
 	echo " "                                                   >> ./img/${STATE}_VasaM/uninstall.bat
 	echo "reg DELETE %KEY%\Families\FAMILY_${COUNTRY_ID} /f"   >> ./img/${STATE}_VasaM/uninstall.bat
+	echo ":end"                                                >> ./img/${STATE}_VasaM/uninstall.bat
 	echo "pause"                                               >> ./img/${STATE}_VasaM/uninstall.bat
 	echo "exit 0"                                              >> ./img/${STATE}_VasaM/uninstall.bat
 
@@ -432,13 +357,15 @@ else
 	cd ..
 
 
-	# Vytvorim info soubory
-	echo "{"                                       > ./img/${STATE}_VasaM.zip.info
-	echo "    \"version\": \"$VERSION\","         >> ./img/${STATE}_VasaM.zip.info
-	echo "    \"timestamp\": \""`date +"%s"`"\""  >> ./img/${STATE}_VasaM.zip.info
-	echo "}"                                      >> ./img/${STATE}_VasaM.zip.info
+	# Vytvorim info soubor
+	echo "{"                                                                                  > ./img/${STATE}_VasaM.info
+	echo "    \"version\": \"$VERSION\","                                                    >> ./img/${STATE}_VasaM.info
+	echo "    \"timestamp\": \""`date -r ./pbf/${STATE}.osm.pbf +%s`"\","                    >> ./img/${STATE}_VasaM.info
+	echo "    \"hashImg\": \""`sha1sum -b ./img/${STATE}_VasaM.img | awk '{print $1}'`"\","  >> ./img/${STATE}_VasaM.info
+	echo "    \"hashZip\": \""`sha1sum -b ./img/${STATE}_VasaM.zip | awk '{print $1}'`"\""   >> ./img/${STATE}_VasaM.info
+	echo "}"                                                                                 >> ./img/${STATE}_VasaM.info
 
-	cp  ./img/${STATE}_VasaM.zip.info ./img/${STATE}_VasaM.img.info  
+	# sha1sum -b CZ_VasaM.img | awk '{print $1}'
 
 fi
 
